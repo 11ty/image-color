0c396faf9c9cb0c9bf2ddf80fd0c5b5eda4d67e4
Zach Leatherman <zachleat@users.noreply.github.com>
2025-02-28 12:55:01 -0600

    Removes @zachleat/get-pixels dep, inlined

--- image-color.js
+++ image-color.js
@@ -2,7 +2,8 @@ import memoize from "memoize";
 import PQueue from 'p-queue';
 import Color from "colorjs.io";
 import debugUtil from "debug";
-import getPixels from "@zachleat/get-pixels";
+import { PNG } from "pngjs";
+import ndarray from "ndarray";
 import { extractColors } from "extract-colors";
 import Cache from "@11ty/eleventy-fetch";
 import Image from "@11ty/eleventy-img";
@@ -38,66 +39,76 @@ export function memoizeJsonToDisk(fn, options = {}) {
 
 export async function getImage(source) {
 	return Image(source, {
+		// PNG is important here
 		formats: ["png"],
 		widths: [50],
 		dryRun: true,
 	});
 }
 
-export async function getColors(source) {
-	return new Promise(async (resolve, reject) => {
-		let stats = await getImage(source);
-
-		debug("Image fetched: %o", source);
-		getPixels(stats.png[0].buffer, "image/png", (err, pixels) => {
+// Thanks to `get-pixels` https://www.npmjs.com/package/get-pixels
+async function handlePNG(data) {
+  var png = new PNG();
+	return new Promise((resolve, reject) => {
+		png.parse(data, function(err, img_data) {
 			if(err) {
-				debug("Error: %o", err);
 				reject(err);
 			} else {
-				debug("`getPixels` success: %o", source);
-				let data = [...pixels.data];
-				let [width, height] = pixels.shape;
-				extractColors({ data, width, height }).then(colors => {
-					debug("`extractColors` success: %o", source);
-					resolve(colors.map(colorData => {
-						let c = new Color(colorData.hex);
-
-						let contrastDark = c.contrast("#000", "WCAG21");
-						let contrastLight = c.contrast("#fff", "WCAG21");
-
-						let alternate;
-						let mode = "unknown";
-						if(contrastDark > 4.5) {
-							// contrasts well with #000
-							alternate = "#000"; // text is black
-							mode = "light";
-						} else if(contrastLight > 4.5) {
-							// contrasts well with #fff
-							alternate = "#fff"; // text is white
-							mode = "dark";
-						}
-
-						return {
-							colorjs: c,
-							original: colorData.hex,
-							background: ""+c.to("oklch"),
-							foreground: alternate,
-
-							mode,
-							contrast: {
-								light: contrastLight,
-								dark: contrastDark,
-							},
-
-							toString() {
-								return ""+c.to("oklch");
-							}
-						}
-					}).filter(entry => Boolean(entry)));
-				}, reject);
+				resolve(ndarray(new Uint8Array(img_data.data),
+					[img_data.width|0, img_data.height|0, 4],
+					[4, 4*img_data.width|0, 1],
+					0))
 			}
-		});
-	});
+		})
+	})
+}
+
+export async function getColors(source) {
+	let stats = await getImage(source);
+	debug("Image fetched: %o", source);
+
+	let pixels = await handlePNG(stats.png[0].buffer);
+	let data = [...pixels.data];
+	let [width, height] = pixels.shape;
+
+	let colors = await extractColors({ data, width, height });
+	debug("`extractColors` success: %o", source);
+
+	return colors.map(colorData => {
+		let c = new Color(colorData.hex);
+
+		let contrastDark = c.contrast("#000", "WCAG21");
+		let contrastLight = c.contrast("#fff", "WCAG21");
+
+		let alternate;
+		let mode = "unknown";
+		if(contrastDark > 4.5) {
+			// contrasts well with #000
+			alternate = "#000"; // text is black
+			mode = "light";
+		} else if(contrastLight > 4.5) {
+			// contrasts well with #fff
+			alternate = "#fff"; // text is white
+			mode = "dark";
+		}
+
+		return {
+			colorjs: c,
+			original: colorData.hex,
+			background: ""+c.to("oklch"),
+			foreground: alternate,
+
+			mode,
+			contrast: {
+				light: contrastLight,
+				dark: contrastDark,
+			},
+
+			toString() {
+				return ""+c.to("oklch");
+			}
+		}
+	}).filter(entry => Boolean(entry));
 }
 
 let fn = memoizeJsonToDisk(getColors);
